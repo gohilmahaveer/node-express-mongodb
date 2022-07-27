4e30e5021a3d2dd2471dee7d35cd022dd73fe9d3
unknown <gohilmahaveer@gmail.com>
2022-07-27 11:39:24 +0530

    Modify Display and Add Content

--- routes/index.js
+++ routes/index.js
@@ -72,43 +72,9 @@ router.get('/add', (req, res, next) => {
 router.post('/addProcess', (req, res, next) => {
 
   const data = new Model({
-    date: req.body.date,
     name: req.body.name,
     city: req.body.city,
-    mobile: req.body.mobile,
-    drsph: req.body.drsph,
-    drcyl: req.body.drcyl,
-    draxis: req.body.draxis,
-    drva: req.body.drva,
-    nrsph: req.body.nrsph,
-    nrcyl: req.body.nrcyl,
-    nraxis: req.body.nraxis,
-    nrva: req.body.nrva,
-    dlsph: req.body.dlsph,
-    dlcyl: req.body.dlcyl,
-    dlaxis: req.body.dlaxis,
-    dlva: req.body.dlva,
-    nlsph: req.body.nlsph,
-    nlcyl: req.body.nlcyl,
-    nlaxis: req.body.nlaxis,
-    nlva: req.body.nlva,
-    purpose: req.body.purpose,
-    quality: req.body.quality,
-    remark: req.body.remark,
-    f1name: req.body.f1name,
-    g1name: req.body.g1name,
-    f1prise: req.body.f1prise,
-    g1prise: req.body.g1prise,
-    f2name: req.body.f2name,
-    g2name: req.body.g2name,
-    f3name: req.body.f3name,
-    g3name: req.body.g3name,
-    f2prise: req.body.f2prise,
-    g2prise: req.body.g2prise,
-    f3prise: req.body.f3prise,
-    g3prise: req.body.g3prise,
-    gst: req.body.gst,
-    total: req.body.total
+    mobile: req.body.mobile
   });
 
   try {
--- routes/model.js
+++ routes/model.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 
 const dataSchema = new mongoose.Schema({
-    date: {
-        required: true,
-        type: Date
-    },
     name: {
         required: true,
         type: String
@@ -15,105 +11,6 @@ const dataSchema = new mongoose.Schema({
     },
     mobile: {
         required: true,
-        type: Number
-    },
-    drsph: {
-        type: String
-    },
-    drcyl: {
-        type: String
-    },
-    draxis: {
-        type: String
-    },
-    drva: {
-        type: String
-    },
-    nrsph: {
-        type: String
-    },
-    nrcyl: {
-        type: String
-    },
-    nraxis: {
-        type: String
-    },
-    nrva: {
-        type: String
-    },
-    dlsph: {
-        type: String
-    },
-    dlcyl: {
-        type: String
-    },
-    dlaxis: {
-        type: String
-    },
-    dlva: {
-        type: String
-    },
-    nlsph: {
-        type: String
-    },
-    nlcyl: {
-        type: String
-    },
-    nlaxis: {
-        type: String
-    },
-    nlva: {
-        type: String
-    },
-    purpose: {
-        type: String
-    },
-    quality: {
-        type: String
-    },
-    remark: {
-        type: String
-    },
-    f1name: {
-        type: String
-    },
-    g1name: {
-        type: String
-    },
-    f1prise: {
-        type: Number
-    },
-    g1prise: {
-        type: Number
-    },
-    f2name: {
-        type: String
-    },
-    g2name: {
-        type: String
-    },
-    f3name: {
-        type: String
-    },
-    g3name: {
-        type: String
-    },
-    f2prise: {
-        type: Number
-    },
-    g2prise: {
-        type: Number
-    },
-    f3prise: {
-        type: Number
-    },
-    g3prise: {
-        type: Number
-    },
-    gst: {
-        type: Number
-    },
-    total: {
         type: Number
     }
 });
